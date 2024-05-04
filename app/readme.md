1066fb4fd3419dd8e927577ed2f479b1be67c08c
Greenie03 <otyazoula@gmail.com>
2024-05-04 12:49:59 +0200

    readme creation

---
+++ app/readme.md
@@ -0,0 +1,27 @@
+# README 
+
+expliquer les updates du projet dans ce readme pour faciliter quand faudra faire le rapport
+
+## DEVELOPPEMENT DE L'APPLICATION
+
+Afin de faciliter le développement de l'application nous avons choisi de voir l'application en trois
+parties distinctes selon les différents cas d'usages. Nous nous retrouvons donc avec les trois
+sous-parties suivantes :
+
+**-Partie anonyme**
+**-Partie candidat**
+**-Partie employeur**
+
+### PART I - PARTIE ANONYME
+
+Nous commencerons donc l'implémentation du code par la partie que l'on jugera la plus simple à 
+développer afin d'avoir rapidement un produit viable et utilisable dans au moins un cas 
+d'utilisation. C'est pour cette raison que nous avons commencé par développer la partie anonyme de 
+l'application, en effet, de par sa nature l'application limite un certain nombre de son usage 
+lorsque l'utilisateur n'est pas connecté (notamment la fonction consulter les messages ou les offres 
+enregistrées) ce qui nous permettra dans un premier lieu de nous concentrer sur les fonctionnalités 
+essentielles a ce cas d'utilisation.
+
+(J'ai essayé de taper une petite intro pour faire genre mais par la suite je vais juste taper le nom
+du commit et ce qui a été fait après on remettra au propre en temps et en heure)
+
--- app/src/main/java/com/example/interimax/MainActivity.java
+++ app/src/main/java/com/example/interimax/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.interimax;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,44 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.navbar);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        /*switch (item.getItemId()) {
+            case R.id.navigation_home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, firstFragment)
+                        .commit();
+                return true;
+
+            case R.id.navigation_message:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, secondFragment)
+                        .commit();
+                return true;
+
+            case R.id.navigation_bookmark:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, thirdFragment)
+                        .commit();
+                return true;
+
+            case R.id.navigation_notifications:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, thirdFragment)
+                        .commit();
+                return true;
+        }*/
+        return false;
     }
 }
